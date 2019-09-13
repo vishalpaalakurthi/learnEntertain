203cdc773fbb6c6c289898274eb7f9048cf8673b
vishalpaalakurthi <vishal.paalakurthi@hdworks.in>
2019-09-13 14:15:07 +0530

    first commit

---
+++ src/main/java/com/java/learnEntertain/LearnEntertainApplication.java
@@ -0,0 +1,13 @@
+package com.java.learnEntertain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnEntertainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnEntertainApplication.class, args);
+	}
+
+}
